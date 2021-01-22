b32089a53a57c2ecaee9fcbd67571857b796ebb1
AlvaroGonzGonz <alvaro.gonz.cua@gmail.com>
2021-01-22 10:16:23 +0100

    Corrected

--- src/main/java/AgeStats.java
+++ src/main/java/AgeStats.java
@@ -1,5 +1,7 @@
+import com.tdunning.math.stats.Histogram;
 import es.upm.etsisi.cf4j.data.DataModel;
 import es.upm.etsisi.cf4j.data.User;
+import es.upm.etsisi.cf4j.util.plot.HistogramPlot;
 
 public class AgeStats {
     public AgeStats(){
@@ -9,40 +11,47 @@ public class AgeStats {
         try {
             DataModel datamodel = DataModel.load("gml1M");
 
-        double userAge, mean, var;
-        double count = 0.0;
-        double sum = 0.0;
-        double max = 0.0;
-        double min = 100.0;
+            HistogramPlot plot = new HistogramPlot("Age", 57);
 
-        for (User user : datamodel.getUsers()){
+            double userAge, mean, var;
+            double count = 0.0;
+            double sum = 0.0;
+            double max = 0.0;
+            double min = 100.0;
+            int count56 = 0;
 
-            userAge = user.getDataBank().getDouble("age");
+            for (User user : datamodel.getUsers()){
+                userAge = (int) 100.0 * user.getDataBank().getDouble("age");
 
-            if(max < userAge) max = userAge;
-            if(min > userAge) min = userAge;
+                plot.addValue(userAge);
 
-            sum += userAge;
-            count ++;
-        }
+                if(max < userAge) max = userAge;
+                if(min > userAge) min = userAge;
 
-        mean = sum / count;
+                sum += userAge;
+                count ++;
+            }
 
-        sum = 0.0;
-        count = 0.0;
+            mean = sum / count;
 
-        for (User user : datamodel.getUsers()){
+            sum = 0.0;
+            count = 0.0;
 
-            userAge = user.getDataBank().getDouble("age");
+            for (User user : datamodel.getUsers()){
 
-            sum += Math.pow((userAge - mean), 2);
-            count ++;
-        }
+                userAge = (int) 100.0 * user.getDataBank().getDouble("age");
 
-        var = Math.sqrt(sum / count);
+                sum += Math.pow((userAge - mean), 2);
+                count ++;
+            }
 
-        System.out.println("Las edades de MovieLens1M se sitúan entre " + min + " años y " + max + " años.");
-        System.out.println("La media de las edades es " + mean + " y su desviación típica es " + var);
+            var = Math.sqrt(sum / count);
+
+            System.out.format("Las edades de MovieLens1M se sitúan entre %d años y %d años. ", (int) min, (int) max);
+            System.out.format("La media de las edades es %.2f y su desviación típica es %.2f. ", mean, var);
+            System.out.println(count56);
+
+            plot.draw();
 
         }catch(Exception e) {
             e.printStackTrace();
